fa8eb0d181291cc1342191ef38197272ddcaae52
Татьяна Кочубейник <kisa7392@mail.ru>
2023-05-03 21:17:32 +0300

    First version

---
+++ Lesson 3/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,43 @@
+﻿#include <vector>
+#include <iostream>
+#include <future>
+#include <algorithm>
+
+
+using namespace std;
+
+void find_min(vector<int> & V, int i, promise<int> prom)
+{
+	int min = i;
+	for (int j = i; j < V.size(); j++) {
+		if (V[j] < V[min]) {
+			min = j;
+		}
+	}
+	prom.set_value(min);
+}
+
+void Async_sort(vector<int>&V) {
+	for (int i = 0; i < V.size(); i++) {
+		promise<int> P1;
+		future<int> F1 = P1.get_future();
+		future<void> res = async(find_min, ref(V), i, move(P1));
+		int res_min = F1.get();
+		if (res_min > i) {
+			swap(V[i], V[res_min]);
+		}
+	}
+}
+
+
+int main()
+{
+    vector<int> V = { 13, 65, 2, 5, 33, 9, 1, 8, 15, 42, 6, 10, 7, 26, 4 };
+	Async_sort(V);
+	cout << "Sort: ";
+	for (auto& x : V) {
+		this_thread::sleep_for(200ms);
+		cout << x << " ";
+	}
+    return 0;
+}
---
+++ Lesson 3/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,45 @@
+﻿#include <future>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template<typename Iterator>
+void parallel_for_each(Iterator first, Iterator last, function<void(int)> func)
+{
+	unsigned long const length = distance(first, last);
+	unsigned long const max_chunk_size = 25;
+	if (length <= max_chunk_size)
+	{
+		for_each(first, last, func);
+	}
+	else
+	{
+		Iterator mid_point = first;
+		advance(mid_point, length / 2);
+		future<void> first_half_result = async(launch::async, parallel_for_each<Iterator>, first, mid_point, func);
+		future<void> second_half_result = async(launch::async, parallel_for_each<Iterator>, mid_point, last, func);
+	}
+}
+
+int main()
+{
+	vector<int> V(1000);
+	generate(V.begin(), V.end(), []() {return rand() % 10; });
+	auto start = chrono::high_resolution_clock::now();
+	for_each(V.begin(), V.end(), [](int i) {i++; });
+	auto end = chrono::high_resolution_clock::now();
+	chrono::duration<double> res = end - start;
+	cout << res.count() << endl;
+
+	vector<int> V1(1000);
+	generate(V1.begin(), V1.end(), []() {return rand() % 10; });
+	auto start1 = chrono::high_resolution_clock::now();
+	parallel_for_each(V1.begin(), V1.end(), [](int i) {i++; });
+	auto end1 = chrono::high_resolution_clock::now();
+	chrono::duration<double> res1 = end1 - start1;
+	cout << res1.count() << endl;
+
+	return 0;
+}
